a52b5bcb558cb0b103d122e6ef1233e2a15cac15
Kangrok Kim <kimpro1010@gmail.com>
2019-03-17 06:23:45 +0900

    Create Matrix_Operation_20190317.R

---
+++ Do You Wanna Fit A Linear Regression Model/Matrix_Operation_20190317.R
@@ -0,0 +1,19 @@
+# practice of matrix operation related with the linear regression analysis
+
+a <- matrix(rep(1:6), ncol=2)
+a
+
+# t() : get the transpose matrix(전치행렬) of x
+b <- t(a)
+b
+
+
+# Use of %*% operator and crossprod()
+
+# 1)
+b %*% a
+crossprod(a)
+
+# 2)
+a %*% b
+crossprod(b)
