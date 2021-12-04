a2034e241286cf9ee2abdf482f66f0fef8d9e299
Kangrok Kim <kimpro@uebermensch.tech>
2021-12-04 21:35:04 +0900

    Update Request.py

--- PublicDataPortal/Request.py
+++ PublicDataPortal/Request.py
@@ -3,11 +3,11 @@
 
 
 import requests
-import Key              # call keys from Key.py
+import Key                                      # call keys from Key.py
 
 url = 'http://apis.data.go.kr/1160100/service/GetBondTradInfoService/getIssuIssuItemStat'
 params = {
-    'serviceKey' : Key.decodingKey,
+    'serviceKey' : Key.decodingKey,             # .encodingKey occurs an error; SERVICE_KEY_IS_NOT_REGISTERED_ERROR
     'pageNo' : '1',
     'numOfRows' : '10',
     'resultType' : 'xml',
@@ -16,5 +16,5 @@ params = {
     'bondIsurNm' : '국동'
 }
 
-response = requests.get(url, params=params)
+response = requests.get(url, params=params)     # require encoding key, not decoding key
 print(response.content)
