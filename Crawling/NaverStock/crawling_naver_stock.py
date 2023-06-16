648c43bd4793c45f4e155ec417b483cae8f40abf
Kangrok Kim <kimpro@uebermensch.tech>
2023-06-16 14:27:29 +0900

    Update crawling_naver_stock.py

--- Crawling/NaverStock/crawling_naver_stock.py
+++ Crawling/NaverStock/crawling_naver_stock.py
@@ -11,63 +11,70 @@ from bs4 import BeautifulSoup
 import aiohttp
 
 
-async def fetch(session, url):
+async def fetch(_session, _url):
 
-    async with session.get(url) as response:
-        return await response.text()
+    async with _session.get(_url) as _response:
+        return await _response.text()
 
 
-async def get_post(session, code, page):
+async def get_post(_session, _code, _page):
 
-    url = f"https://finance.naver.com/item/board.naver?code={code}&page={page}"
-    time_list = []
-    time_pattern = r"(\d{4})\.(\d{2})\.(\d{2}) (\d{2}):(\d{2})"
+    _url = f"https://finance.naver.com/item/board.naver?code={_code}&page={_page}"
+    _time_list = []
+    _title_list = []
 
-    html = await fetch(session, url)
-    soup = BeautifulSoup(html, "html.parser")
-
-    spans = soup.select(".section.inner_sub table tbody tr span")
+    _html = await fetch(_session, _url)
+    _soup = BeautifulSoup(_html, "html.parser")
+    _spans1 = _soup.select(".section.inner_sub table tbody tr td span")
+    _spans2 = _soup.select(".section.inner_sub table tbody tr td[class='title'] a")
 
     if TEST:
-        print(url)                                                              # Ok
-        # print(soup)                                                           # Ok
-        # pprint.pprint(spans)                                                  # Ok
+        print(_url)                                                             # Ok
+        # print(_soup)                                                          # Ok
+        pprint.pprint(_spans2)
 
-    for span in spans:
-        match = re.search(time_pattern, span.text)
-        if match:
-            time_list.append(match.group())
+    _time_pattern = r"(\d{4})\.(\d{2})\.(\d{2}) (\d{2}):(\d{2})"
+    for _span in _spans1:
+        _match = re.search(_time_pattern, _span.text)
+        if _match:
+            _time_list.append(_match.group())
 
-    return code, page, time_list
+    for _span in _spans2:
+        _title_list.append(_span['title'])
+
+    _time_title_list = list(zip(_time_list, _title_list))
+
+    return _code, _page, _time_title_list
 
 
 async def main():
 
     if TEST:
-        codes = ["005930"]
-        pages = ["1"]
+        _codes = ["005930"]
+        _pages = ["1"]
     else:
-        codes = ["005930", "373220", "000660"]                                  # 삼성전자, LG에너지솔루션, SK하이닉스
-        pages = ["1", "2", "3"]
+        _codes = ["005930", "373220", "000660"]                                 # 삼성전자, LG에너지솔루션, SK하이닉스
+        _pages = ["1", "2", "3"]
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.115'}
-    # headers = {'User-Agent': 'Edg/109.0.1518.115'}                            # doesn't work but don't know why
-    async with aiohttp.ClientSession(headers=headers) as session:
-        tasks = []
-        for code in codes:
-            for page in pages:
-                task = asyncio.create_task(get_post(session, code, page))
-                tasks.append(await task)
+    _headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.115'}
+    # _headers = {'User-Agent': 'Edg/109.0.1518.115'}                           # doesn't work but don't know why
+    async with aiohttp.ClientSession(headers=_headers) as _session:
+        _tasks = []
+        for _code in _codes:
+            for _page in _pages:
+                _task = asyncio.create_task(get_post(_session, _code, _page))
+                _tasks.append(await _task)
 
-        return tasks
+        return _tasks
 
 
 if __name__ == "__main__":
 
-    TEST = True
+    TEST = False
     loop = asyncio.get_event_loop()
     tasks = loop.run_until_complete(main())
 
     for task in tasks:
-        code, page, time_list = task
-        pprint.pprint(f"종목: {code}, 페이지: {page}, 게시일시: {time_list}")
+        code, page, time_title_list = task
+        for time_title in time_title_list:
+            print(f"종목: {code}, 페이지: {page}, 게시일시: {time_title[0]}, 제목: {time_title[1]}")
